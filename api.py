ff7cba51600ed49946742a959a7cb78d282b9fb6
Alexander Hunter <52008357+alex-hunter3@users.noreply.github.com>
2020-08-22 22:41:46 +0100

    Add files via upload

---
+++ api.py
@@ -0,0 +1,29 @@
+import flask
+
+from bs4 import BeautifulSoup
+from requests import get
+
+
+def get_case_by_country(country="TOTAL"):
+    pass
+
+
+def get_page():
+    return get("https://www.worldometers.info/coronavirus/").text
+
+
+def get_total_cases():
+	page = get_page()
+
+	soup = BeautifulSoup(page, "html.parser")
+	divs = soup.findAll("div", {"class": "maincounter-number"})
+
+	span = divs[0].findAll("span")
+	span = str(span[0])
+	span = span.replace('<span style="color:#aaa">', "")
+	span = span.replace(' </span>', '')
+
+	return span
+
+
+print(get_total_cases())
