3a0fc35a49bc76b60e0541cf475f0a0ada8122cc
Alexander Hunter <52008357+alex-hunter3@users.noreply.github.com>
2020-08-22 23:17:03 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,20 +1,32 @@
-import flask
-
+from flask import Flask, render_template
 from bs4 import BeautifulSoup
 from requests import get
 
 
-def get_case_by_country(country="TOTAL"):
-    pass
+app = Flask(__name__)
+
+
+def get_cases_by_country(country="total"):
+    if country == "total":
+    	return get_total_cases(get_page())
+    else:
+    	page = get_page_by_country(country.lower())
+
+    	try:
+    		return get_total_cases(page)
+    	except IndexError:
+    		return None
 
 
 def get_page():
     return get("https://www.worldometers.info/coronavirus/").text
 
 
-def get_total_cases():
-	page = get_page()
+def get_page_by_country(country):
+	return get(f"https://www.worldometers.info/coronavirus/country/{country.strip().lower()}").text
 
+
+def get_total_cases(page):
 	soup = BeautifulSoup(page, "html.parser")
 	divs = soup.findAll("div", {"class": "maincounter-number"})
 
@@ -24,3 +36,13 @@ def get_total_cases():
 	span = span.replace(' </span>', '')
 
 	return span
+
+
+@app.route("/")
+def total_cases():
+	return f"{get_total_cases(get_page())}"
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+	
