c1a5ca4d2ef4eb664c24c27e1d58ebfeb277c674
Alexander Hunter <52008357+alex-hunter3@users.noreply.github.com>
2020-08-23 09:19:42 +0100

    Update api.py

--- api.py
+++ api.py
@@ -6,16 +6,28 @@ from requests import get
 app = Flask(__name__)
 
 
-def get_cases_by_country(country="total"):
+def get_cases_by_country(country, page):
     if country == "total":
     	return get_total_cases(get_page())
     else:
-    	page = get_page_by_country(country.lower())
-
     	try:
     		return get_total_cases(page)
     	except IndexError:
     		return None
+
+
+def get_deaths_by_country(country, page):
+    try:
+    	return get_total_deaths(page)
+    except IndexError:
+    	return None
+
+
+def get_recoveries_by_country(country, page):
+	try:
+		return get_total_recoveries(page)
+	except IndexError:
+		return None
 
 
 def get_page():
@@ -31,16 +43,56 @@ def get_total_cases(page):
 	divs = soup.findAll("div", {"class": "maincounter-number"})
 
 	span = divs[0].findAll("span")
-	span = str(span[0])
-	span = span.replace('<span style="color:#aaa">', "")
-	span = span.replace(' </span>', '')
+	span = str(span[0].text)
 
-	return span
+	span = span.replace(",", "")
+
+	try:
+		return span.strip()
+	except AttributeError:
+		return span
+
+
+def get_total_deaths(page):
+	soup = BeautifulSoup(page, "html.parser")
+	divs = soup.findAll("div", {"class": "maincounter-number"})
+
+	span = divs[1].findAll("span")
+	span = str(span[0].text)
+
+	span = span.replace(",", "")
+
+	try:
+		return span.strip()
+	except AttributeError:
+		return span
+
+
+def get_total_recoveries(page):
+	soup = BeautifulSoup(page, "html.parser")
+	divs = soup.findAll("div", {"class": "maincounter-number"})
+
+	span = divs[2].findAll("span")
+	span = str(span[0].text)
+
+	span = span.replace(",", "")
+
+	try:
+		return span.strip()
+	except AttributeError:
+		return span
 
 
 @app.route("/")
-def total_cases():
-	return f"{get_total_cases(get_page())}"
+def total_data():
+	page = get_page()
+	return f"{get_total_cases(page)},{get_total_deaths(page)},{get_total_recoveries(page)}"
+
+
+@app.route("/<country>")
+def total_data_by_country(country):
+	page = get_page_by_country(country)
+	return f"{get_cases_by_country(country, page)},{get_deaths_by_country(country, page)},{get_recoveries_by_country(country, page)}"
 
 
 if __name__ == "__main__":
