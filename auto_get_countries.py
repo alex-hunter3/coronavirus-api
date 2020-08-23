61056f9a10d70e1c3460c73f1a732835a12a14d1
Alexander Hunter <52008357+alex-hunter3@users.noreply.github.com>
2020-08-23 10:48:57 +0100

    Add files via upload

---
+++ auto_get_countries.py
@@ -0,0 +1,30 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+
+def get_page():
+	return get("https://www.worldometers.info/coronavirus/").text
+
+
+def get_tags(page):
+	countries = []
+	soup = BeautifulSoup(page, "html.parser")
+	countries_ = soup.find_all("a", {"class": "mt_a"}, href=True)
+
+	for country in countries_:
+		countries.append(country["href"])
+
+	return countries
+
+
+def write_file(countries):
+	file = open("countries.txt", "w")
+
+	for country in countries:
+		print("/" + country)
+		file.write("/" + str(country) + "\n")
+
+	file.close()
+
+
+write_file(get_tags(get_page()))
