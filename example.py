759c7b00e96a3630a41f34c08086c0e08b803d39
Alexander Hunter <52008357+alex-hunter3@users.noreply.github.com>
2020-08-24 20:59:14 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,15 @@
 # example program to get data
 import corona
 
-# main function to return list [cases, deaths, recovered]
-print(str(corona.get_country_data()) + "\n")
+# main function to return list [cases, deaths, recovered] of world data
+print(corona.total_data())
+
+# function to return country specific data
+# returns all items as strings if there is an "N/A" in the list otherwise will be int list
+print(corona.total_data_by_country("uk"))
 
 # function returns valid country names that can be used in the program
 print(str(corona.valid_countries()) + "\n")
 
 # function takes in a string to check if the string passed in is a valid country name
-print(str(corona.check_valid("us")) + "\n")
+print(str(corona.check_valid("uk")) + "\n")
